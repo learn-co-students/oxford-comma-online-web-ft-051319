ff1029711de4c289098d231c31615dae836bbd3a
voluble-bug-0219 <montensia1@outlook.com>
2019-05-14 21:08:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+      array.join
+  elsif array.size == 2
+      array.join(" and ")
+  else
+      last_string = array.pop
+      joinedString = array.join(", ")
+      "#{joinedString}, and #{last_string}"
+  end
+  
 end
